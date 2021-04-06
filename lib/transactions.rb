94ae512178c4c62f8b6cc127333d3338d316adc1
ianmcnicholas <ianjmcnicholas@.com>
2021-04-06 15:45:18 +0100

    credit method added to Transactions class.  This class can now build a credit transaction

--- lib/transactions.rb
+++ lib/transactions.rb
@@ -9,6 +9,11 @@ class Transactions
     @history = []
   end
 
+  def credit(amount, balance)
+    new_record = {date: date_of_transaction, credit: amount, balance: balance}
+    @history.push(new_record)
+  end
+
   def date_of_transaction
     Time.now.strftime("%d/%m/%Y")
   end
--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -14,6 +14,12 @@ describe Transactions do
     it 'can create the date in dd/mm/yy format' do
       expect(transaction.date_of_transaction).to eq(Time.now.strftime("%d/%m/%Y"))
     end
+
+    it 'can build a credit transaction' do
+      transaction.credit(500, 500)
+      expect(transaction.history).to eq([{date: Time.now.strftime("%d/%m/%Y"), credit: 500, balance: 500}])
+    end
+
   end
 
 end
