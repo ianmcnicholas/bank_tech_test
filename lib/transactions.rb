c99fb1a1ac00f94956c3478d6ebc94f9d3197330
ianmcnicholas <ianjmcnicholas@.com>
2021-04-06 15:37:29 +0100

    date_of_transaction method added to Transaction class to produce dd/mm/yy of transaction

--- lib/transactions.rb
+++ lib/transactions.rb
@@ -1,9 +1,16 @@
 class Transactions
 
+  # time = Time.new
+  # time.strftime("%d/%m/%Y")        # "05/12/2015"
+
   attr_reader :history
 
   def initialize
     @history = []
+  end
+
+  def date_of_transaction
+    Time.now.strftime("%d/%m/%Y")
   end
 
 end
--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -2,15 +2,18 @@ require 'transactions'
 
 describe Transactions do
 
-  let(:transactions) { Transactions.new }
+  let(:transaction) { Transactions.new }
 
   context 'upon creation:' do
-
     it 'should initialise with an empty history' do
-      expect(transactions.history).to eq([])
+      expect(transaction.history).to eq([])
     end
-
   end
 
+  context 'creating a transaction' do
+    it 'can create the date in dd/mm/yy format' do
+      expect(transaction.date_of_transaction).to eq(Time.now.strftime("%d/%m/%Y"))
+    end
+  end
 
 end
